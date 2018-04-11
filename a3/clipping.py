03aaf3e474599b425febec5f3b837189b9d67b52
khersey <kmhersey951@gmail.com>
2018-04-11 13:30:19 -0400

    updated A3 to match A4

--- a3/clipping.py
+++ a3/clipping.py
@@ -19,14 +19,21 @@ def clip(face_list):
                 vertices.append(find_intersection((n,m)))
                 vertices.append(n)
 
-        if len(vertices) != 3:
-            # entire thing outside
-            pass
-        else:
+        if len(vertices) == 3:
             face.v1 = vertices[0]
             face.v2 = vertices[1]
             face.v3 = vertices[2]
             new_face_list.append(face)
+        elif len(vertices) == 4:
+            # TODO: change order to make phong shading work
+            face.v1 = vertices[0]
+            face.v2 = vertices[1]
+            face.v3 = vertices[2]
+            new_face_list.append(face)
+            face2 = Face(vertices[0], vertices[2], vertices[3], face.rgb)
+            new_face_list.append(face2)
+
+            
 
     return new_face_list
 
@@ -35,9 +42,9 @@ def is_inside(vertex):
     y = vertex[1]
     z = vertex[2]
 
-    x_in = x <= 1 and x >= -1
-    y_in = y <= 1 and y >= -1
-    z_in = z <= 1 and z >= 0
+    x_in = x <= 1.0 and x >= -1.0
+    y_in = y <= 1.0 and y >= -1.0
+    z_in = z <= 1.0 and z >= 0.0
 
     res = x_in and y_in and z_in
     return res
@@ -46,9 +53,9 @@ def find_intersection(edge):
     inside = edge[0]
     outside = edge[1]
 
-    if not (outside[0] <= 1 and outside[0] >= -1): # x outside
+    if not (outside[0] <= 1.0 and outside[0] >= -1.0): # x outside
         new_x = 1.0 
-        if outside[0] < -1:
+        if outside[0] < -1.0:
             new_x = -1.0
         
         scale = (new_x - inside[0]) / (outside[0] - inside[0])
@@ -58,9 +65,9 @@ def find_intersection(edge):
 
         outside = (new_x, new_y, new_z)
 
-    if not (outside[1] <= 1 and outside[1] >= -1):
+    if not (outside[1] <= 1.0 and outside[1] >= -1.0):
         new_y = 1.0 
-        if outside[1] < -1:
+        if outside[1] < -1.0:
             new_y = -1.0
         
         scale = (new_y - inside[1]) / (outside[1] - inside[1])
@@ -70,9 +77,9 @@ def find_intersection(edge):
 
         outside = (new_x, new_y, new_z)
 
-    if not (outside[2] <= 1 and outside[2] >= 0):
+    if not (outside[2] <= 1.0 and outside[2] >= 0.0):
         new_z = 1.0 
-        if outside[2] < 0:
+        if outside[2] < 0.0:
             new_z = 0.0
         
         scale = (new_z - inside[2]) / (outside[2] - inside[2])
